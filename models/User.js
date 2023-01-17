41bb10e97c3cda91b5f0bb288ac3f1df435b407d
Darius Garcia <dariusgarcia888@gmail.com>
2023-01-16 22:56:50 -0800

    added: User model

---
+++ models/User.js
@@ -0,0 +1,38 @@
+const { Schema, model } = require('mongoose')
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    posts: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'Post',
+      },
+    ],
+    friends: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+      },
+    ],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+    id: false,
+  }
+)
+
+const User = model('User', userSchema)
+
+module.exports = User
