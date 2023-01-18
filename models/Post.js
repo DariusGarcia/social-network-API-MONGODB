19de1a196b1f62f6230848afbca72d2195e618dd
Darius Garcia <dariusgarcia888@gmail.com>
2023-01-17 19:15:09 -0800

    added: Post model

---
+++ models/Post.js
@@ -0,0 +1,32 @@
+const { Schema, model } = require('mongoose')
+const reactionSchema = require('./Reaction')
+
+const postSchema = new Schema(
+  {
+    bodyText: {
+      type: String,
+      required: 'Post cannot be empty!',
+      minlength: 1,
+      maxlength: 256,
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
+    reactions: [reactionSchema],
+  },
+  {
+    toJSON: {
+      getters: true,
+    },
+    id: false,
+  }
+)
+
+const Post = model('Post', postSchema)
+
+module.exports = Post
